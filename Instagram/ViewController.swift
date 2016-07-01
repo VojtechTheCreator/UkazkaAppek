c4f88f62cf11240c8c5c1fecfd4a7b062efbb0fd
Vojtěch Honig <vojtechhonig@gmail.com>
2016-07-01 10:58:37 +0200

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Vojtěch Honig on 01.07.16.
+//  Copyright © 2016 Vojtěch Honig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
